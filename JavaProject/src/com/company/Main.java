5a078cf86f503d3cf7c9fe9c4a62717e984b007d
terry9011 <terrienyam@yahoo.com>
2021-09-23 22:38:26 +0100

    task 13

---
+++ JavaProject/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("hello world");
+    System.out.println(70);
+        System.out.println("Ruth"+ "Nyam");
+        
+    }
+}
+
+
