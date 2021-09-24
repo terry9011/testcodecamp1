9c5725637cad3a1eda13fa5e2d4a7430b41152b6
terry9011 <terrienyam@yahoo.com>
2021-09-24 23:25:23 +0100

    BasicArithmeticOps

---
+++ src/com/company/JavaDataType.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class JavaDataType{
+    public static void main(String[] args) {
+
+
+        byte age= 72;
+                short money= 500;
+                long cash= 1000l;
+                float lenght= 123.5f;
+                double area=12678.3;
+        System.out.println(area);
+        char option='n';
+
+        boolean drink= true;
+        System.out.println( money);
+
+    }}
---
+++ src/com/company/StringAndConcat.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class StringAndConcat {
+    public static void main(String[] args) {
+        //strings and concatenation
+
+        String bird = " a dove";
+        String action =" is gentle";
+        //System.out.println(bird + action);
+        //string and primitive concat
+        String movie = "king of boys";
+        int part =   4;
+        //System.out.println(movie + part);
+        // .concat method
+
+       // System.out.println(movie.concat(part+""));
+
+        // task 15
+        int age = 15;
+        String name = "angel";
+        // method 1
+        System.out.println(name + age);
+        //method 2
+        System.out.println(name.concat(age+""));
+
+
+
+    }
+
+
+
+}
