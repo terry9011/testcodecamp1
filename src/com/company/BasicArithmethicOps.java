295fcc681d8b1ed04f28f11ad7993c5116120fc9
terry9011 <terrienyam@yahoo.com>
2021-09-24 23:25:23 +0100

    BasicArithmeticOps

---
+++ src/com/company/BasicArithmethicOps.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class BasicArithmethicOps {
+    public static void main(String[] args) {
+        //basic arithmetic
+        byte age1 = 25;
+        byte age2 = 30;
+        //System.out.println(age1+age2);
+        //System.out.println(age1-age2);
+        //System.out.println(age1*age2);
+        //incremental and decremantal
+        age1++;
+        age2--;
+        //System.out.println(age1);
+        //System.out.println(age2);
+        //compound assigngment operators
+        age1 += age2;
+        //System.out.println(age1);
+        String area= "area of circle";
+        double pie = 3.142;
+        double rad = 12.7;
+
+        System.out.println("area"+"=" + pie*rad*rad);
+
+
+
+
+    }
+
+
+}
