535a9600b0734b0a7153d1089abf966e7a7a1e7b
terry9011 <terrienyam@yahoo.com>
2021-09-26 17:03:31 +0100

    CondRelLogStatements task 17

--- src/com/company/CondRelLogStatements.java
+++ src/com/company/CondRelLogStatements.java
@@ -5,16 +5,21 @@ public class CondRelLogStatements {
 
         // age qualification
         int myAge = 30;
-        int mynumber=20;
-        if(mynumber>20) {
+        int mynumber=50;
+        if(mynumber % 3==0) {
             System.out.println("fizz");
-        }else if (mynumber==20 && mynumber<10)
-            System.out.println("BUZZ");
-        else System.out.println("fizzbuzz");
+        }else if(mynumber % 5== 0){
+            System.out.println("buzz");
+        }else if (mynumber %5==0  && mynumber % 3==0) {
+            System.out.println("FIZZBUZZ");
+        }
+        else System.out.println("fizzbuzz cannot be applied");
 
 
-        //if(myAge != 30 || myAge<35){
+
+        if(myAge != 30 || myAge<35){
             //System.out.println("not qualified");
+        }
         }
        // else if (myAge<=25){
             //System.out.println("qualified");
