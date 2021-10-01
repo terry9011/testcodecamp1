3c8c6a4d2bda309ba30ef96d7662e97a85a9607b
terry9011 <terrienyam@yahoo.com>
2021-10-01 18:01:37 +0100

    while   loops task21b

--- src/com/company/WhileLoops.java
+++ src/com/company/WhileLoops.java
@@ -6,8 +6,8 @@ public class WhileLoops {
     public static void main(String[] args) {
         //Write a logic that takes in input from the user. It keeps printing "try again "until the user enters "testify"
         Scanner terry= new Scanner(System.in);
-        String UserInput = "";
 
+        String UserInput = "";
         while (!UserInput.equalsIgnoreCase("testify")){
             System.out.println("welcome , \"try again \" to exit this loop");
             UserInput   =  terry.nextLine();
