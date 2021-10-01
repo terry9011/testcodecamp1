40d8ba1c2807e70b32ba5d93d94ed010dd8f3827
terry9011 <terrienyam@yahoo.com>
2021-10-01 17:59:17 +0100

    ForLoops task21b

---
+++ src/com/company/WhileLoops.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class WhileLoops {
+    public static void main(String[] args) {
+        //Write a logic that takes in input from the user. It keeps printing "try again "until the user enters "testify"
+        Scanner terry= new Scanner(System.in);
+        String UserInput = "";
+
+        while (!UserInput.equalsIgnoreCase("testify")){
+            System.out.println("welcome , \"try again \" to exit this loop");
+            UserInput   =  terry.nextLine();
+        }
+
+
+    }
+}
